83729ded6c9b7b945f7d49ba9885049d3ce0fc76
DM-08 <56652167+DM-08@users.noreply.github.com>
2023-03-25 14:10:21 +0000

    Add project files.

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+namespace GymStats.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string text;
+    }
+}
